b9cb4859685e3e9bfab7eac75a48632dce37b72d
Ash Chang <asichang@ucdavis.edu>
2024-04-24 19:19:26 -0700

    cat

---
+++ studio2/script.js
@@ -0,0 +1,40 @@
+let globalData;
+async function getData(){
+    const meows = await fetch('data.json');
+    const data = await meows.json();
+    console.log(data);
+    globalData = data;
+    document.querySelector('#meowCount').innerHTML = outputHTML1(data);
+    document.querySelector('#picker').innerHTML = createSelectList(data);
+}
+
+function outputHTML1(data){
+    let html = '<p>';
+    html += `Keeping count of how many times I meow`;
+    html += '</p>';
+    return html;
+}
+
+function createSelectList(data){
+    let html = '<option>---</option>';
+    const dataPoints = Object.keys(data);
+    console.log(dataPoints);
+    dataPoints.forEach(function(eachPoint){
+        html += `<option value = "${eachPoint}">${data[eachPoint].day}</option>`
+    });
+    return html;
+}
+
+document.querySelector('#picker').addEventListener('change', function(){
+    const newValue = this.value;
+    updateInterface(newValue, globalData);
+});
+
+function updateInterface(value, jsonData){
+    let html = '<p>';
+    html += `On ${jsonData[value].day}, I meowed <b>${jsonData[value].meows}</b> times.`;
+    html += '</p>';
+    document.querySelector('#result').innerHTML = html;
+}
+
+getData();
